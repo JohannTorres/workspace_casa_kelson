e275ae9de5a43c52e01c983b36d08a0e60adf5ca
Johann Torres <johann.backend@gmail.com>
2024-12-10 17:50:43 -0300

    atualizacaoInserirListarComAxios

--- aula05/src/App.jsx
+++ aula05/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AdicionarAluno from './components/AdicionarAluno'
+import ListarAlunos from './components/ListarAlunos'
 
 
 const App = () => {
   return (
     <div>
       <AdicionarAluno/>
+      <ListarAlunos />
     </div>
   )
 }
